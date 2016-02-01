cca069da8757390c139693ce5eba71e990a93dde
Léo Baecker <leo.baecker@epitech.eu>
2016-02-01 13:47:46 +0100

    Renamed to jvstalker-backend

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # JvStalker Backend
-JvStalker, moteur de recherche pour les forums de jeuxvideo.com
+JvStalker, moteur de recherche pour les forums de jeuxvideo.com.
 
 # Installing the Application
 cd to the root folder of the app, then 'npm install' to install dependencies.
---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+
+// Set up ============================
+var express      = require('express');
+var app          = express();
+var mongoose     = require('mongoose');
+var bodyParser   = require('body-parser');
+
+// Configuration =====================
+var db = require('./config/db');
+
+// Set the port
+var port = process.env.PORT || 8080;
+
+// Connect to the database
+mongoose.connect(db.url);
+
+// Parse the app to json
+app.use(bodyParser.json());
+
+app.listen(8080);
+console.log("App listening to port 8080");
